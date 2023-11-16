89cfa57bc741cb66b54d7ecb174c8c8889faf379
alexjames4 <alexanderjamesmansfield@gmail.com>
2023-11-16 18:10:05 +0000

    score displayed, hitting enter button after gameover or completion restarts the game

--- src/brickbreaker/Gameplay.java
+++ src/brickbreaker/Gameplay.java
@@ -21,10 +21,12 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private int ballposY = 350; //starting pos of ball
     private int ballXdir = -1; //ball x direction
     private int ballYdir = -2; //ball y direction
+    private MapGenerator map;
 
 
     //constructor
     public Gameplay() {
+        map = new MapGenerator(3,7);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -37,11 +39,19 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         g.setColor(Color.black);
         g.fillRect(1,1, 692, 592);
 
+        //drawing map
+        map.draw((Graphics2D)g);
+
         //borders
         g.setColor(Color.yellow);
         g.fillRect(0, 0, 3, 592);
         g.fillRect(0, 0, 692, 3);
         g.fillRect(684, 0, 3, 592);
+
+        //score display
+        g.setColor(Color.white);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString(String.valueOf(score), 590, 30);
 
         //paddle
         g.setColor(Color.green);
@@ -50,6 +60,32 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         //ball
         g.setColor(Color.orange);
         g.fillOval(ballposX, ballposY, 20, 20);
+
+        if(totalBricks <= 0) {
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("You won", 260, 300);
+
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Press Enter to restart", 250, 350);
+        }
+
+        if(ballposY > 570) {
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("Game Over, Score: " + score, 190, 300);
+
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Press Enter to restart", 250, 350);
+        }
 
         g.dispose();
     }
@@ -60,6 +96,35 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
             //intersection with paddle
             if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
                 ballYdir = -ballYdir;
+            }
+
+            //intersection with bricks
+            A: for(int i=0; i < map.map.length; i++) {
+                for (int j = 0; j < map.map[0].length; j++) {
+                    if (map.map[i][j] > 0) {
+                        int brickX = j * map.brickWidth + 80;
+                        int brickY = i * map.brickHeight + 50;
+                        int brickWidth = map.brickWidth;
+                        int brickHeight = map.brickHeight;
+
+                        Rectangle brickRect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+                        Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+
+                        if(ballRect.intersects(brickRect)) {
+                            map.setBrickValue(0, i, j);
+                            totalBricks--;
+                            score += 5;
+
+                            if(ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width) {
+                                ballXdir = -ballXdir;
+                            } else {
+                                ballYdir = - ballYdir;
+                            }
+
+                            break A;
+                        }
+                    }
+                }
             }
 
             ballposX += ballXdir;
@@ -99,6 +164,22 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
                 playerX = 10; //this keeps the slider in the game screen
             } else {
                 moveLeft();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (!play) {
+                play = true;
+                ballposX = 120;
+                ballposY = 350;
+                ballXdir = -1;
+                ballYdir = -2;
+                playerX = 310;
+                score = 0;
+                totalBricks = 21;
+
+                map = new MapGenerator(3,7);
+
+                repaint();
             }
         }
     }
